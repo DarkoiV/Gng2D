b1c629220c1e0cd8746b3e50576c5c4f9e4e7d58
Maksymilian <makkob997@gmail.com>
2023-08-21 19:44:27 +0200

    Added sandbox

---
+++ sandbox/main.cpp
@@ -0,0 +1,24 @@
+#include "Gng2D/core/application.hpp"
+#include "Gng2D/scene/scene.hpp"
+
+struct SandboxScene : Gng2D::Scene
+{
+};
+
+struct Sandbox : Gng2D::Application
+{
+    void onCreate() override 
+    {
+        setNextScene<SandboxScene>();
+    }
+
+    void onQuit() override 
+    {
+        stopRunning();
+    }
+};
+
+int main()
+{
+    Sandbox().run();
+}
