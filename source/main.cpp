d7e0d5ab6044eff115cf5490dd0962bb387630ee
Maksymilian <makkob997@gmail.com>
2023-10-25 03:16:25 +0200

    Added lua

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "lua.hpp"
 
 int main()
 {
-    std::cout << "Hello Gng2D" << std::endl;
+    auto* L = luaL_newstate();
+    luaL_dostring(L, "x = 10");
+    lua_getglobal(L, "x");
+    double x = lua_tonumber(L, -1);
+    std::cout << "Hello Gng2D: " << x << std::endl;
     return 0;
 }
