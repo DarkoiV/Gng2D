8ee5e5a71b4f6b1f40f1a394aa40e9627db8dd25
Maksymilian <makkob997@gmail.com>
2023-10-26 20:44:55 +0200

    Lua files for test is generated programmatically

--- source/core/tst/luna.cpp
+++ source/core/tst/luna.cpp
@@ -1,28 +1,53 @@
 #include "Gng2D/core/luna.hpp"
 #include "gtest/gtest.h"
+#include <filesystem>
+#include <fstream>
 
-static std::string TEST_DIRECTORY()
-{
-    std::string s{__FILE__};
-    auto cutFrom = s.find_last_of('/');
-    return s.substr(0, cutFrom+1);
-}
+constexpr char INT_ID[]         = "TEST_INT";
+constexpr int INT_VALUE         = 129;
+constexpr char FLOAT_ID[]       = "TEST_FLOAT";
+constexpr double FLOAT_VALUE    = 129.389;
+constexpr char BOOL_ID[]        = "TEST_BOOL";
+constexpr bool BOOL_VALUE       = false;
+constexpr char STR_ID[]         = "TEST_STRING";
+constexpr char STR_VALUE[]      = "HELLO LUNA!";
 
 struct LunaTest : ::testing::Test 
 {
-    inline static std::string testFile{TEST_DIRECTORY() + "test.lua"};
+    inline static std::string testFilesDir{TEST_FILES_DIR};
+    inline static std::string testFileName{"test.lua"};
+    inline static std::filesystem::path testFile{testFilesDir + "/" + testFileName};
+    static void SetUpTestSuite()
+    {
+        std::fstream file(testFile, std::fstream::out);
+        std::boolalpha(file);
+        file << INT_ID      << " = " << INT_VALUE   << "\n";
+        file << FLOAT_ID    << " = " << FLOAT_VALUE << "\n";
+        file << BOOL_ID     << " = " << BOOL_VALUE  << "\n";
+        file << STR_ID      << " = " << std::quoted(STR_VALUE) << std::endl;
+    }
 
     Gng2D::Luna luna;
 };
 
 TEST_F(LunaTest, AfterDoingFile_LunaCanReadGlobalVariables)
 {
-    luna.doFile(testFile);
+    luna.doFile(testFile.string());
 
-    ASSERT_EQ(*luna.readGlobalInt("TEST_INT"), 123);
-    ASSERT_EQ(*luna.readGlobalFloat("TEST_FLOAT"), 123.321);
-    ASSERT_EQ(*luna.readGlobalBool("TEST_BOOL"), false);
-    ASSERT_EQ(*luna.readGlobalString("TEST_STRING"), "HELLO LUA!");
+    auto readInt    = luna.readGlobalInt(INT_ID);
+    auto readFloat  = luna.readGlobalFloat(FLOAT_ID);
+    auto readBool   = luna.readGlobalBool(BOOL_ID);
+    auto readString = luna.readGlobalString(STR_ID);
+
+    ASSERT_TRUE(readInt);
+    ASSERT_TRUE(readFloat);
+    ASSERT_TRUE(readBool);
+    ASSERT_TRUE(readString);
+
+    ASSERT_EQ(*readInt, INT_VALUE);
+    ASSERT_EQ(*readFloat, FLOAT_VALUE);
+    ASSERT_EQ(*readBool, BOOL_VALUE);
+    ASSERT_EQ(*readString, STR_VALUE);
 }
 
 TEST_F(LunaTest, AfterDoingFile_LunaCanUseReadToVar)
@@ -34,14 +59,14 @@ TEST_F(LunaTest, AfterDoingFile_LunaCanUseReadToVar)
     bool        testBool;
     std::string testString;
 
-    luna.readToVar("TEST_INT", testInt);
-    luna.readToVar("TEST_FLOAT", testFloat);
-    luna.readToVar("TEST_BOOL", testBool);
-    luna.readToVar("TEST_STRING", testString);
+    luna.readToVar(INT_ID, testInt);
+    luna.readToVar(FLOAT_ID, testFloat);
+    luna.readToVar(BOOL_ID, testBool);
+    luna.readToVar(STR_ID, testString);
 
-    ASSERT_EQ(testInt, 123);
-    ASSERT_EQ(testFloat, 123.321);
-    ASSERT_EQ(testBool, false);
-    ASSERT_EQ(testString, "HELLO LUA!");
+    ASSERT_EQ(testInt, INT_VALUE);
+    ASSERT_EQ(testFloat, FLOAT_VALUE);
+    ASSERT_EQ(testBool, BOOL_VALUE);
+    ASSERT_EQ(testString, STR_VALUE);
 }
 
