904c741e3cac7a5d0efcdf8ccc0a1eba4ef9b3fb
Maksymilian <makkob997@gmail.com>
2023-10-28 03:49:33 +0200

    InputHandler actions are now triggered only by correct key

--- source/scene/src/input_handler.cpp
+++ source/scene/src/input_handler.cpp
@@ -10,6 +10,7 @@ InputHandler::InputHandler(entt::registry& r)
     : reg(r)
 {
     keyPressActions[SDL_SCANCODE_UP] = "up"_hs;
+    keyPressActions[SDL_SCANCODE_W]  = "up"_hs;
 }
 
 InputHandler::~InputHandler()
@@ -20,10 +21,13 @@ void InputHandler::handleKeyPress(SDL_KeyboardEvent& e)
 {
     if(e.repeat) return;
 
+    auto action = keyPressActions.find(e.keysym.scancode);
+    if (action == keyPressActions.end()) return;
+
     auto view = reg.view<KeyPress>();
-    view.each([this](entt::entity e, KeyPress& ki)
+    view.each([this, action](entt::entity e, KeyPress& ki)
     {
-        auto it = std::lower_bound(ki.handlers.begin(), ki.handlers.end(), "up"_hs);
+        auto it = std::lower_bound(ki.handlers.begin(), ki.handlers.end(), action->second);
         if (it != ki.handlers.end())
         {
             auto& inputHandler = *it;
